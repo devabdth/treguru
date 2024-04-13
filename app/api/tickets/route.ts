c3f1dc1f9861ec20e03bdd5df4a884d7061dcfeb
devabdth <a.tharwat@cubersio.com>
2024-04-13 03:09:25 +0200

    Tickets Route

--- app/api/tickets/route.ts
+++ app/api/tickets/route.ts
@@ -1,20 +1,35 @@
 import { NextResponse, NextRequest } from "next/server";
-import fs from "fs";
-import path from "path";
+import DatabaseHelper from "@/database";
+import { ITicket } from "@/models/ticket";
+import { authMiddleware } from "@/app/middleware";
 
+const dbHelper: DatabaseHelper= new DatabaseHelper();
 
-export const GET= async (req: NextRequest, res: NextResponse) => {
-    return NextResponse.json(401, {
+export const GET= async (req: NextRequest, _: NextResponse) => {
+    const authRes= authMiddleware(req);
+    if (!authRes) return NextResponse.json(401);
+    const tickets: Array<ITicket> | undefined = await dbHelper.tickets!.getAllTickets();
+    return NextResponse.json(JSON.stringify({tickets: tickets}), {
         headers: {
             "Content-Type": "application/json"
         }
     })
 }
 
-export const POST= async (req: NextRequest, res: NextResponse) => {
-    return NextResponse.json(401, {
-        headers: {
-            "Content-Type": "application/json"
-        }
-    })
+export const POST= async (req: NextRequest, _: NextResponse) => {
+    try {
+        const body= await req.json();
+        if (!body) return NextResponse.json(401, { headers: { "Content-Type": "application/json" } });
+
+        const ticketRes= await dbHelper.tickets!.createTicket(body!);
+
+        return NextResponse.json((ticketRes ? 201 : 401), {
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+    } catch(e) {
+        console.log(e);
+        return NextResponse.json(500);
+    }
 }
