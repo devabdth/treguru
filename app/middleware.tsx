8ad7a24a234272e68921b9e579653e7b6bfcbd8e
devabdth <a.tharwat@cubersio.com>
2024-04-13 03:08:57 +0200

    API Requests Auth Middleware

---
+++ app/middleware.tsx
@@ -0,0 +1,12 @@
+import { NextResponse, type NextRequest } from "next/server";
+import * as dotenv from "dotenv";
+
+
+export function authMiddleware(req: NextRequest) {
+    dotenv.config();
+    const authKey= process.env.API_KEY;
+
+    const auth= req.headers.get("Authorization");
+    return !(authKey === undefined || auth !== authKey)
+}
+
