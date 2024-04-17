e0209e8f278398f878f6df0b2f2e5f17423c7393
devabdth <a.tharwat@cubersio.com>
2024-04-17 14:22:10 +0200

    Configurations

--- database/index.ts
+++ database/index.ts
@@ -1,12 +1,16 @@
 import TicketsHelper from "@/database/tickets";
+import ProjectsHelper from "./projects";
+
 import { connect } from "mongoose";
 class DatabaseHelper{
     tickets?: TicketsHelper;
+    projects?: ProjectsHelper;
     constructor() {
         connect(process.env.DB_URL!, {
             useNewUrlParser: true, useUnifiedTopology: true,
-        }) 
+        })
         this.tickets= new TicketsHelper();
+        this.projects= new ProjectsHelper();
     }
 
 }
