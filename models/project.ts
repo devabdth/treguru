a9d54d46e9160b95d4ac58738d62594827a5a323
devabdth <a.tharwat@cubersio.com>
2024-04-17 14:19:28 +0200

    Project Model

---
+++ models/project.ts
@@ -0,0 +1,25 @@
+import { Schema, Document, Types, model, models } from "mongoose"
+
+interface IProject extends Document {
+    _id: Types.ObjectId;
+    title: string;
+    bio: string;
+    projectId: string;
+    backgroundImage: string;
+    projectDisplayImages: Array<string>;
+    portfolioSectionId: string,
+}
+
+const ProjectSchema= new Schema<IProject>({
+    title: {type: String, required: true},
+    bio: {type: String, required: true},
+    projectId: {type: String, required: true},
+    backgroundImage: {type: String, required: true},
+    portfolioSectionId: {type: String, required: true},
+    projectDisplayImages: {type: Array<string>, required: true},
+});
+
+export type {IProject, ProjectSchema}
+
+const Ticket= models.Project || model<IProject>('Project', ProjectSchema);
+export default Ticket;
