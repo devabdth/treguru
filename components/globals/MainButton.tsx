b78668c93bb3bd1e0d5b782b07682d7a385c6e6c
devabdth <a.tharwat@cubersio.com>
2024-04-17 14:24:25 +0200

    New Variant

--- components/globals/MainButton.tsx
+++ components/globals/MainButton.tsx
@@ -30,11 +30,11 @@ const MainButton = ({
       type={type}
       className={`${
         cursor ?? "cursor-pointer"
-      } flexCenter ${!(variant === "icon-only") && "px-2 py-1 gap-2"} rounded-xl ${variant} ${customClassesStyle ?? ""}`}
+      } flexCenter ${!(variant === "icon-only") && "px-2 py-1 gap-2"} rounded-xl ${(variant === "shadow") && "bg-transparent"} ${variant} ${customClassesStyle ?? ""}`}
     >
       {icon && <Image src={icon} alt={title} width={iconWidth ?? 24} height={iconHeight??24}></Image>}
       <label
-        className={`${cursor ?? "cursor-pointer"} bold-16 whitespace-nowrap`}
+        className={`${cursor ?? "cursor-pointer"} whitespace-nowrap ${(variant !== "shadow") ? "bold-16": "text-white underline regular-12"}`}
       >
         {title}
       </label>
