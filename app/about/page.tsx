63e30c6919d21c5baf6a65f7a3c5e4fd160387f3
devabdth <a.tharwat@cubersio.com>
2024-04-17 14:22:31 +0200

    About Page: Route

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,11 +1,20 @@
-import { HeroSection, CountsSection, VisionSection } from "@/components/about"
+import { HeroSection, ServicesSection, FeaturesSection } from "@/components/about"
+import { DEFAULT_KEYWORDS, DEFAULT_DESCRIPTION } from "@/prefs/seo";
+
+import type { Metadata } from "next";
+export const metadata: Metadata = {
+    title: 'TreGuru - About',
+    keywords: DEFAULT_KEYWORDS,
+    description: DEFAULT_DESCRIPTION,
+};
+
 
 const AboutPage = () => {
   return (
     <main>
       <HeroSection />
-      <CountsSection />
-      <VisionSection />
+      <ServicesSection />
+      <FeaturesSection />
     </main>
   )
 }
