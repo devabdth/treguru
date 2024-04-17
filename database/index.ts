c9760c1689ea317e71fc1729d0875b7da278ce72
devabdth <a.tharwat@cubersio.com>
2024-04-17 19:36:38 +0200

    Specify Connect Options Type

--- database/index.ts
+++ database/index.ts
@@ -1,14 +1,14 @@
 import TicketsHelper from "@/database/tickets";
 import ProjectsHelper from "./projects";
 
-import { connect } from "mongoose";
+import { connect, ConnectOptions } from "mongoose";
 class DatabaseHelper{
     tickets?: TicketsHelper;
     projects?: ProjectsHelper;
     constructor() {
         connect(process.env.DB_URL!, {
             useNewUrlParser: true, useUnifiedTopology: true,
-        })
+        } as ConnectOptions)
         this.tickets= new TicketsHelper();
         this.projects= new ProjectsHelper();
     }
