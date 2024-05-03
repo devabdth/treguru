8b374af97703dd17a4538eb9a9c1c11c174fee40
devabdth <a.tharwat@cubersio.com>
2024-05-03 16:20:14 +0300

    Add Reels Database Helper

--- database/index.ts
+++ database/index.ts
@@ -2,15 +2,17 @@ import TicketsHelper from "@/database/tickets";
 import ProjectsHelper from "./projects";
 
 import { connect, ConnectOptions } from "mongoose";
+import ReelsHelper from "./reels";
+
 class DatabaseHelper{
     tickets?: TicketsHelper;
     projects?: ProjectsHelper;
+    reels: ReelsHelper;
     constructor() {
-        connect(process.env.DB_URL!, {
-            useNewUrlParser: true, useUnifiedTopology: true,
-        } as ConnectOptions)
+        connect(process.env.DB_URL!, {})
         this.tickets= new TicketsHelper();
         this.projects= new ProjectsHelper();
+        this.reels= new ReelsHelper();
     }
 
 }
